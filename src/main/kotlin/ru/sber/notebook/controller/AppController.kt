ec067e6ac18fd52074152586331224f809f8eca4
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-25 01:58:50 +0300

    AppController complete

--- src/main/kotlin/ru/sber/notebook/controller/AppController.kt
+++ src/main/kotlin/ru/sber/notebook/controller/AppController.kt
@@ -3,19 +3,69 @@ package ru.sber.notebook.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
+import ru.sber.notebook.NoteModel
+import ru.sber.notebook.service.NotebookService
 
 @Controller
 @RequestMapping("/app")
-class AppController {
-    @GetMapping("/greeting")
-    fun greeting(
-        @RequestParam(name = "name", required = false, defaultValue = "World") name: String?,
-        model: Model
-    ): String? {
-        model.addAttribute("name", name)
-        return "greeting"
+class AppController @Autowired constructor(val notebookService: NotebookService) {
+
+    @GetMapping("/add")
+    fun addNote(model: Model): String?{
+        model.addAttribute("name", "Добавление записи")
+
+        return "add'n'edit"
+    }
+
+    @GetMapping("/{id}/edit")
+    fun editNote(@PathVariable id: String, model: Model): String?{
+        model.addAttribute("name", "Измение записи $id")
+
+        return "add'n'edit"
+    }
+
+    @GetMapping("/list")
+    fun getNoteByQuery(@RequestParam query: Map<String, String>, model: Model): String {
+        val searchResult = notebookService.getByQuery(query)
+
+        model.addAttribute("name", "Список записей по запросу")
+        model.addAttribute("result", searchResult)
+
+        return "list"
+    }
+
+    @GetMapping("/{id}")
+    fun getNoteById(@PathVariable id: String, model: Model): String {
+        val searchResult = notebookService.getById(id.toInt())
+
+        model.addAttribute("name", "Запись номер $id")
+        model.addAttribute("result", searchResult)
+
+        return "list"
+    }
+
+    @GetMapping("/{id}/delete")
+    fun deleteNote(@PathVariable id: String, model: Model): String {
+        notebookService.delNote(id.toInt())
+        model.addAttribute("result", "Записи больше нет")
+
+        return "result"
+    }
+
+    @PostMapping("/add")
+    fun addNote(@ModelAttribute noteModel: NoteModel, model: Model): String {
+        notebookService.addNote(noteModel)
+        model.addAttribute("result", "Запись добавлена")
+
+        return "result"
+    }
+
+    @PostMapping("/{id}/edit")
+    fun editNote(@PathVariable id: String, @ModelAttribute noteModel: NoteModel, model: Model): String {
+        notebookService.editNote(id.toInt(), noteModel)
+        model.addAttribute("result", "Запись изменена")
+
+        return "result"
     }
 }
