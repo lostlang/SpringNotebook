1013600e953e97c5170cf23abed7ba333bf46bb1
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-24 23:16:48 +0300

    Login Servlet complete

--- src/main/kotlin/ru/sber/notebook/servlet/LoginServlet.kt
+++ src/main/kotlin/ru/sber/notebook/servlet/LoginServlet.kt
@@ -1,13 +1,34 @@
 package ru.sber.notebook.servlet
 
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import java.math.BigInteger
+import java.security.MessageDigest
 import javax.servlet.annotation.WebServlet
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-@WebServlet(name = "loginServlet")
-@RequestMapping("/login")
-class LoginServlet {
+@WebServlet(name = "loginServlet", urlPatterns = ["/login"])
+class LoginServlet: HttpServlet() {
+    private val usernameAdmin = "lostlang"
+    private val passwordAdmin = "21232f297a57a5a743894a0e4a801fc3" // admin
 
+    fun md5(input:String): String {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    }
+
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        req!!.getRequestDispatcher("/login.html").forward(req, resp)
+    }
+
+    override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        if (req?.getParameter("username") == usernameAdmin && md5(req.getParameter("password")) == passwordAdmin) {
+            val cookie = Cookie("auth", passwordAdmin)
+            resp!!.addCookie(cookie)
+            resp.sendRedirect("/menu.html")
+        } else {
+            resp!!.sendRedirect("/login.html")
+        }
+    }
 }
