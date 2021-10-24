9ec0caba8d6ea1fe669d1255ee0375b4bef44caa
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-24 22:56:37 +0300

    Add Servlet

--- src/main/kotlin/ru/sber/notebook/NotebookApplication.kt
+++ src/main/kotlin/ru/sber/notebook/NotebookApplication.kt
@@ -2,8 +2,10 @@ package ru.sber.notebook
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
 @SpringBootApplication
+@ServletComponentScan
 class NotebookApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/ru/sber/notebook/servlet/LoginServlet.kt
@@ -0,0 +1,13 @@
+package ru.sber.notebook.servlet
+
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.annotation.WebServlet
+
+@WebServlet(name = "loginServlet")
+@RequestMapping("/login")
+class LoginServlet {
+
+}
